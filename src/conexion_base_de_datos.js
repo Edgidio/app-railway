0b15ef48688e4004c63969e764dd1069750194bb
Edgidio Leon <edgidio.leon@gmail.com>
2022-09-24 00:54:34 -0400

    Listo.2

--- src/conexion_base_de_datos.js
+++ src/conexion_base_de_datos.js
@@ -4,13 +4,14 @@ const mysql = require('mysql2');
 const {promisify} = require('util');
     // creando la conexion pool
 const pool = mysql.createPool({
-    host: process.env.host_db || 'localhost',
+    host: process.env.host_db || 'containers-us-west-69.railway.app',
     user: process.env.user_db || 'root',
-    password: process.env.password_db || '1q2w3e4rt',
+    password: process.env.password_db || 'iC8BafIA6ffdqMsvPOye',
     database: process.env.name_database_db || 'railway_ed',
     ssl: {
         rejectUnauthorized: false
-    }
+    },
+    port: process.env.PORT_db
 });
 
 // Extablecer la nonexion a la base de datos
